a09c4799308531b8a2b33e15d99d01ad154c8ce2
MarcoC-S117 <marcocastillo82@hotmail.com>
2023-09-06 16:37:01 -0600

    PDA works for all symbols

--- src/App.java
+++ src/App.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("1_2 Programa: PDA");
+        System.out.println("L = { w | w is a palindrome of a's and b's}");
+        System.out.println("\nDefinicion Formal: \nM = {Q, E, ¬, &, q0, Z0, F} donde");
+        System.out.println("Q = {q0, q1}\nE = {A, B}\n¬ = {S, 0, 1}\nF = {}\nZ0={S}\nq0=q0\n");
+
         System.out.println("Ingrese una cadena para analizar: ");
         String cadena = scan.nextLine();
         if (!cadena.isEmpty()) {
