365640357421b7650d790f62e8eae3b3c915ade5
MarcoC-S117 <marcocastillo82@hotmail.com>
2023-09-06 15:33:08 -0600

    PDA Works

--- src/App.java
+++ src/App.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Ingrese una cadena para analizar: ");
-        String cadena = System.console().readLine();
-        if(cadena==""){
-            PDA pda = new PDA(cadena);
-        }else System.out.println("La cadena no puede estar vacia");        
+        String cadena = scan.nextLine();
+        if (!cadena.isEmpty()) {
+            if (cadena.length() % 2 != 1) { // Verificar si la longitud es impar
+                PDA pda = new PDA(cadena);
+                System.out.println(pda.Transitions() ? "Es palíndromo" : "No es palíndromo");
+            } else {
+                System.out.println("La cadena no puede tener longitud impar");
+            }
+        } else {
+            System.out.println("La cadena no puede estar vacía");
+        }
+        
+        
+         
+        scan.close();     
     }
 }
--- src/PDA.java
+++ src/PDA.java
@@ -8,32 +8,34 @@ public class PDA {
     PDA(String Cadena){
         Mitad_Cadena = Cadena.length()/2;
         this.cadena = Cadena;
-        this.pila = new PDAPila(Mitad_Cadena);
+        this.pila = new PDAPila(Mitad_Cadena+1);
     }
 
     boolean Transitions(){
-        boolean valid = false;
-        for(int i=0; i<cadena.length();i++){
+        boolean valid = false;        
+        for(int i=0; i<cadena.length();i++){        
             switch (state){
                 case 0:
-                    if(i<Mitad_Cadena){
+                    if(i<Mitad_Cadena){                        
                         state=0;
-                        pila.push(cadena.charAt(i));
-                    }else state = 1;
-
-                    
-                case 1:
-                    if(i!=cadena.length() && pila.top()== cadena.charAt(i)){
+                        pila.push(cadena.charAt(i));       
+                    }else {state = 1; i--;}
+                break;
+                case 1:                    
+                    if(pila.empty()){
+                        System.out.println("Es palindromo");
+                    }
+                    else if(i!=cadena.length() && pila.top()== cadena.charAt(i)){
                         state=1;                        
                         pila.pop();
-                    } else if(pila.top()!= cadena.charAt(i)){
-                        System.out.println("No es palindromo y no existe transicion para el simbolo" + cadena.charAt(i));
-                    } else if(i==cadena.length() && pila.empty()){
-                        System.out.println("Es palindromo");
+                    } else if (pila.top()!= cadena.charAt(i)){
+                        System.out.println("No es palindromo y no existe transicion para el simbolo: " + cadena.charAt(i));
+                        return false;
+                    } 
                     valid=true;
-                    }
-            }        
-        }
+                    break;
+                    }                   
+            }         
         return valid;
     }
 }
